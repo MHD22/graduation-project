9ae66391d7462ad496dd4adc40aebaa800628edd
MHD22 <mhd.sad22@gmail.com>
2020-11-21 14:52:56 +0200

    login page is done

--- graduation-project/src/components/Login.js
+++ graduation-project/src/components/Login.js
@@ -1,14 +1,47 @@
 import React from 'react' ;
+import { BrowserRouter as Router,Link} from 'react-router-dom';
 
 
 function Login(){
+
+    const checkId=(e)=>{
+        e.preventDefault();
+        const form = document.getElementById('signInForm');
+        // form.preventDefault();
+        console.log(form.teacherId.value,form.teacherPassword.value);
+        fetch('http://localhost:3000/loginTeacher',{
+            method: 'POST',
+            headers: {
+            'Content-Type': 'application/json',
+            },
+            body: JSON.stringify({id: form.teacherId.value , password:form.teacherPassword.value }),
+        }).then(res => res.json())
+        .then(data=>{
+            if(data.length === 1){
+                const {firstName,lastName,id_number} = data[0];
+                const storedData = {firstName,lastName,id_number};
+                sessionStorage.setItem('teacher',JSON.stringify(storedData));
+
+                window.location.reload();
+            }
+        })
+        .catch("error during send student data to backend");
+
+    
+    };
     return(
         <>
         <h1 className="main-title">Login Page</h1>
-        <form className="mt-3">
-            <input type="text" placeholder="Student ID .." className="form-input mt-4" />
+        <form className="mt-3" id='signInForm' >
+            <input name='teacherId' type="text" placeholder="Teacher ID .." className="form-input mt-4" required/>
+            <br/>
+            <input name='teacherPassword' type="password" placeholder="Password.." className="form-input mt-4" required/>
             <br />
-            <input type="submit" id='submit' value="Login" className="btn btn-success btn-submit mt-4" />
+            <Router>
+                <Link exact to="/">
+                <input type="submit" id='submit' onClick={checkId} value="Login" className="btn btn-success btn-submit mt-4"  />
+                </Link>
+            </Router>
         </form>
         </>
     );
--- graduation-project/src/components/NavBar.js
+++ graduation-project/src/components/NavBar.js
@@ -1,24 +1,49 @@
-import React from 'react' ;
+import React, { Component } from 'react' ;
 import { Navbar , Nav } from 'react-bootstrap';
 import {NavLink} from 'react-router-dom' ;
 
-function NavBar(){
-    return (
-    <>
-        <Navbar bg="dark" variant="dark">
-            <Navbar.Brand href='/' className="brand-font">Graduation Project</Navbar.Brand>
-            <Nav className="mr-auto">
-                    <NavLink className="ml-3 nav-font p-1" exact to="/">Home</NavLink>
-                    <NavLink className="ml-3 nav-font p-1" to="/about">About US</NavLink>
-                    <NavLink className="ml-3 nav-font p-1" to="/contact">Contact</NavLink>
-                    <NavLink className="ml-3 nav-font p-1" to="/show">My Classes</NavLink>
-                    <NavLink className="ml-3 nav-font p-1" to="/create">Add Class</NavLink>
-            </Nav>
-            <NavLink className="ml-3 nav-font text-light p-1" to="/register">Register</NavLink>
-            <NavLink className="ml-3 nav-font text-light p-1" to="/Login">Login</NavLink>
-        </Navbar>
-    </>
-    )
+class NavBar extends Component{
+    constructor(){
+        super();
+        this.state={
+            teacherName:'',
+            login: false ,
+        }
+    }
+    componentDidMount(){
+        const data = JSON.parse(sessionStorage.getItem('teacher')) || '';
+        if(data){
+            const name =`${data.firstName} ${data.lastName}`;
+            this.setState({teacherName:name, login:true});
+
+        }
+        else{
+            this.setState({teacherName:'',login:false});
+        }
+    }
+    
+    
+   
+    render(){
+        
+        return (
+        <>
+            <Navbar id="navbar" bg="dark" variant="dark">
+                <Navbar.Brand href='/' className="brand-font">Graduation Project</Navbar.Brand>
+                <Nav className="mr-auto">
+                        <NavLink className="ml-3 nav-font p-1" exact to="/">Home</NavLink>
+                        <NavLink className="ml-3 nav-font p-1" to="/about" ></NavLink>
+                        <NavLink className="ml-3 nav-font p-1" to="/contact">Contact</NavLink>
+                        <NavLink className="ml-3 nav-font p-1" to="/show">My Classes</NavLink>
+                        <NavLink className="ml-3 nav-font p-1" to="/create">Add Class</NavLink>
+                </Nav>
+                <NavLink className="ml-3 nav-font text-light p-1" to="/register" hidden={this.state.login}>Register</NavLink>
+                <NavLink className="ml-3 nav-font text-light p-1" to="/Login" hidden={this.state.login}>Login</NavLink>
+        <NavLink className="ml-3 nav-font text-light p-1" to="/Login" hidden={!this.state.login}>{this.state.teacherName}</NavLink>
+            </Navbar>
+        </>
+        )
+    }
 }
 
 export default NavBar ;
