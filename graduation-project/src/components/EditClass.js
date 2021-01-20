45e2891f99682fd509369fc148795007bb46784e
Mazen-Al-Samman <mazenalsmman@gmail.com>
2021-01-20 18:02:26 +0200

    Add sweet alert for all pages

--- graduation-project/src/components/EditClass.js
+++ graduation-project/src/components/EditClass.js
@@ -4,27 +4,28 @@ import { Col, Row } from 'react-bootstrap';
 import { Redirect } from 'react-router-dom';
 import AddStudents from './AddStudents';
 import Swal from 'sweetalert2'
+import './CreateClass.css';
 
 class EditClass extends Component {
-    constructor(){
+    constructor() {
         super();
-        this.state={
-            rows:[],
-            clickedAdd:false,
-            isThereChanges:false,
-            students:[],
-            lastStateOfStudents:[],
-            route:null
+        this.state = {
+            rows: [],
+            clickedAdd: false,
+            isThereChanges: false,
+            students: [],
+            lastStateOfStudents: [],
+            route: null
         }
     }
 
-    componentDidMount(){
-        let students =[].concat(this.props.classInfo.students);
+    componentDidMount() {
+        let students = [].concat(this.props.classInfo.students);
         this.renderTable(students);
-        this.setState({students});
+        this.setState({ students });
     }
-    
-    renderTable=(students)=>{
+
+    renderTable = (students) => {
         let rows = students.map((std) => {
             return (
                 <tr id={`r${std.id}`} key={std.id}>
@@ -34,23 +35,30 @@ class EditClass extends Component {
                 </tr>
             )
         });
-        this.setState({rows});
+        this.setState({ rows });
     }
 
-    deleteStd=(id)=> {
-        Swal.fire({
+    deleteStd = (id) => {
+        const swalWithBootstrapButtons = Swal.mixin({
+            customClass: {
+                confirmButton: 'btn btn-success',
+                cancelButton: 'btn btn-danger'
+            },
+            buttonsStyling: false
+        })
+
+        swalWithBootstrapButtons.fire({
             title: 'Are you sure?',
-            text: 'User will have Admin Privileges',
+            text: "You won't be able to revert this!",
             icon: 'warning',
             showCancelButton: true,
-            confirmButtonColor: '#3085d6',
-            cancelButtonColor: '#d33', 
-            confirmButtonText: 'Yes!'
-        })
-        .then((result) => {
-            if (result.value) {
-                let changedList =[].concat(this.state.lastStateOfStudents);
-                let lastStateOfStudents = changedList.length? changedList : [].concat(this.state.students); 
+            confirmButtonText: 'Yes, delete it!',
+            cancelButtonText: 'No, cancel!',
+            reverseButtons: true
+        }).then((result) => {
+            if (result.isConfirmed) {
+                let changedList = [].concat(this.state.lastStateOfStudents);
+                let lastStateOfStudents = changedList.length ? changedList : [].concat(this.state.students);
                 lastStateOfStudents = lastStateOfStudents.filter(std => {
                     return std.id !== id;
                 })
@@ -58,125 +66,145 @@ class EditClass extends Component {
                 rows = rows.filter(std => std.key !== id);
                 this.setState({
                     rows,
-                    isThereChanges:true,
+                    isThereChanges: true,
                     lastStateOfStudents
                 });
+                swalWithBootstrapButtons.fire(
+                    'Deleted!',
+                    'Your file has been deleted.',
+                    'success'
+                )
+            } else if (
+                result.dismiss === Swal.DismissReason.cancel
+            ) {
+                swalWithBootstrapButtons.fire(
+                    'Cancelled',
+                    'Your imaginary file is safe :)',
+                    'error'
+                )
             }
         })
+
     }
 
-    addStd=(arr)=>{
-        let changedList =[].concat(this.state.lastStateOfStudents);
-        let lastStateOfStudents = changedList.length? changedList : [].concat(this.state.students); 
-        let idsOfCurrentStudents = lastStateOfStudents.map(obj=>obj.id).join(' ');
+    addStd = (arr) => {
+        let changedList = [].concat(this.state.lastStateOfStudents);
+        let lastStateOfStudents = changedList.length ? changedList : [].concat(this.state.students);
+        let idsOfCurrentStudents = lastStateOfStudents.map(obj => obj.id).join(' ');
         let isThereChanges = this.state.isThereChanges;
         arr.forEach(std => {
-            if( ! idsOfCurrentStudents.includes(std.id)){
+            if (!idsOfCurrentStudents.includes(std.id)) {
                 lastStateOfStudents.push(std);
                 this.renderTable(lastStateOfStudents);
-                isThereChanges=true;
+                isThereChanges = true;
             }
         });
         this.setState({
-            clickedAdd:false,
+            clickedAdd: false,
             isThereChanges,
             lastStateOfStudents
         });
     }
 
-    onAddStd=()=>{
-        this.setState({clickedAdd:true});
+    onAddStd = () => {
+        this.setState({ clickedAdd: true });
     }
 
-    onDeleteClass=()=>{
+    onDeleteClass = () => {
         Swal.fire({
             title: 'Are you sure?',
-            text: 'User will have Admin Privileges',
+            html: "<h5 class='text-danger'>You won't to delete this class !</h5>",
             icon: 'warning',
             showCancelButton: true,
             confirmButtonColor: '#3085d6',
-            cancelButtonColor: '#d33', 
-            confirmButtonText: 'Yes!'
-        })
-        .then((result) => {
-            if(result.value){
-                let classData={
-                    className:this.props.classInfo.className
+            cancelButtonColor: '#d33',
+            confirmButtonText: 'Yes, delete it!'
+        }).then((result) => {
+            if (result.isConfirmed) {
+                let classData = {
+                    className: this.props.classInfo.className
                 }
-                let url='http://localhost:3000/deleteClass';
-                let requestOptions={
+                let url = 'http://localhost:3000/deleteClass';
+                let requestOptions = {
                     method: 'DELETE',
                     headers: {
-                    'Content-Type': 'application/json',
-                },
-                body: JSON.stringify(classData),
+                        'Content-Type': 'application/json',
+                    },
+                    body: JSON.stringify(classData),
                 }
                 fetch(url, requestOptions)
-                .then(res=>res.json())
-                .then(console.log)
-                .catch(e=>{console.log("error on Delete class request..",e)});
-                this.setState({route:'/'});
+                    .then(res => res.json())
+                    .then(console.log)
+                    .catch(e => { console.log("error on Delete class request..", e) });
+                this.setState({ route: '/' });
+                Swal.fire(
+                    'Deleted!',
+                    'Class has been deleted.',
+                    'success'
+                )
             }
         })
     }
 
-    onConfirm=()=>{
+    onConfirm = () => {
         Swal.fire({
-            title: 'Are you sure?',
-            text: 'User will have Admin Privileges',
-            icon: 'warning',
+            title: 'Do you want to save the changes?',
+            showDenyButton: true,
             showCancelButton: true,
-            confirmButtonColor: '#3085d6',
-            cancelButtonColor: '#d33', 
-            confirmButtonText: 'Yes!'
-        })
-        .then((result) => {
-            if(result.value){
-                let classData={
-                    className:this.props.classInfo.className,
-                    students : this.state.lastStateOfStudents
+            confirmButtonText: `Save`,
+            denyButtonText: `Don't save`,
+            icon: 'info'
+        }).then((result) => {
+            if (result.isConfirmed) {
+                let classData = {
+                    className: this.props.classInfo.className,
+                    students: this.state.lastStateOfStudents
                 }
-                let url='http://localhost:3000/editClass';
-                let requestOptions={
+                let url = 'http://localhost:3000/editClass';
+                let requestOptions = {
                     method: 'PUT',
                     headers: {
-                    'Content-Type': 'application/json',
-                },
-                body: JSON.stringify(classData),
+                        'Content-Type': 'application/json',
+                    },
+                    body: JSON.stringify(classData),
                 }
                 fetch(url, requestOptions)
-                .then(res=>res.json())
-                .then(console.log)
-                .catch(e=>{console.log("error on edit class request..",e)});
+                    .then(res => res.json())
+                    .then(console.log)
+                    .catch(e => { console.log("error on edit class request..", e) });
 
                 this.setState({
-                    isThereChanges:false,
-                    lastStateOfStudents:[],
-                    route:'/'
+                    isThereChanges: false,
+                    lastStateOfStudents: [],
+                    route: '/'
                 });
+                Swal.fire('Saved!', '', 'success')
+            } else if (result.isDenied) {
+                this.onCancel();
+                Swal.fire('Changes are not saved', '', 'info')
             }
         })
     }
 
-    onCancel=()=>{
+    onCancel = () => {
         this.renderTable(this.state.students);
         this.setState({
-        isThereChanges:false,
-        lastStateOfStudents:[]
-    });
+            isThereChanges: false,
+            lastStateOfStudents: []
+        });
     }
 
     back = () => {
         this.setState({
-            route:'/show'
+            route: '/show'
         });
     }
 
-    render(){
-        let {rows} = this.state;
+    render() {
+        let { rows } = this.state;
         return (
             <>
-                {this.state.route? <Redirect to={this.state.route}/> : null}
+                {this.state.route ? <Redirect to={this.state.route} /> : null}
                 <img className="mt-2 grow pointer" onClick={this.back} src="https://img.icons8.com/fluent/48/000000/circled-left.png" alt="go back" />
                 <h2 className="f2 mt4 ">{this.props.classInfo.className}</h2>
                 <>
@@ -194,13 +222,13 @@ class EditClass extends Component {
                     </table>
                 </>
 
-                { ! this.state.clickedAdd && (<button className="btn f3 grow btn-warning br4 shadow" onClick={this.onAddStd}>Add Students</button>)}
+                { !this.state.clickedAdd && (<button className="btn f3 grow btn-warning br4 shadow" onClick={this.onAddStd}>Add Students</button>)}
                 {   this.state.clickedAdd && (<AddStudents createNew={false} onAddStd={this.addStd} />)}
-               
+
                 { this.state.isThereChanges && (
-                    <Row className="mv5 justify-content-center"  xs={1} md={2}>
-                        <Col xs={12}  md={3}><button className="ma2 btn f3 grow btn-success br4 shadow" onClick={this.onConfirm}>Confirm Changes</button></Col>
-                        <Col xs={12}  md={3}><button className="ma2 btn f3 grow btn-danger  br4 shadow " onClick={this.onCancel}>Cancel </button></Col>
+                    <Row className="mv5 justify-content-center" xs={1} md={2}>
+                        <Col xs={12} md={3}><button className="ma2 btn f3 grow btn-success br4 shadow" onClick={this.onConfirm}>Confirm Changes</button></Col>
+                        <Col xs={12} md={3}><button className="ma2 btn f3 grow btn-danger  br4 shadow " onClick={this.onCancel}>Cancel </button></Col>
                     </Row>
                 )}
                 <Row className="ma3 justify-content-start"  >
